56f098776358c1bb4ae70b727467f3bae24caf4d
XanDev <165346969+XanDev17@users.noreply.github.com>
2024-10-31 19:18:17 +0000

    Add files via upload

---
+++ src/bmi/BMI.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bmi;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ALEX ASANTE OBENG
+ */
+public class BMI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          
+       
+     MainBoy mainboy = new MainBoy();
+     mainboy.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+     mainboy.setVisible(true);
+     
+        // TODO code application logic here
+    }
+    
+}
